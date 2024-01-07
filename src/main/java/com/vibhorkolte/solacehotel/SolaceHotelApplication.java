b03f6b0d9b04639b104ff2bc484d849753a7b3ce
Vibhor-Kolte <vibhorkolte@gmail.com>
2024-01-07 09:32:32 +0530

    [Local] - Spring Initializer Project Setup

---
+++ src/main/java/com/vibhorkolte/solacehotel/SolaceHotelApplication.java
@@ -0,0 +1,13 @@
+package com.vibhorkolte.solacehotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolaceHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolaceHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vibhorkolte/solacehotel/SolaceHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vibhorkolte.solacehotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolaceHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
